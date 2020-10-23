5911f4dc015f863e1c09c0671f6a50d8ed275372
KushwahaVikashJ <kushwahavicky743@gmail.com>
2020-10-23 22:13:49 +0530

    initial commit

---
+++ backend/data.js
@@ -0,0 +1,77 @@
+export default {
+
+    products:[
+        {
+            _id:'1',
+            name: 'Slim-Shirt',
+            category:'shirts',
+            image: '/images/d2.jpg',
+            price: 60,
+            brand: 'Nike',
+            rating: 2.5,
+            status:'In stock',
+            numReviews: 10,
+            countInStock:6
+        },
+        {
+            _id:'2',
+            name: 'Fit-Shirt',
+            category:'shirts',
+            image: '/images/d3.jpg',
+            price: 80,
+            brand: 'Nike',
+            rating: 4.5,
+            status:'In stock',
+            numReviews: 10,
+            countInStock:5
+        },
+        {
+            _id:'3',
+            name: 'Slim-Pant',
+            category:'pants',
+            image: '/images/d4.jpg',
+            price: 40,
+            brand: 'Nike',
+            rating: 1.5,
+            status:'In stock',
+            numReviews: 10,
+            countInStock:4
+        },
+        {
+            _id:'4',
+            name: 'Fit-Pant',
+            category:'pants',
+            image: '/images/d5.jpg',
+            price: 90,
+            brand: 'Nike',
+            rating: 4.5,
+            status:'In stock',
+            numReviews: 10,
+            countInStock:3
+        },       
+        {
+            _id:'5',
+            name: 'Fit-Shirt',
+            category:'shirts',
+            image: '/images/d3.jpg',
+            price: 80,
+            brand: 'Nike',
+            rating: 4.5,
+            status:'In stock',
+            numReviews: 10,
+            countInStock:5
+        },
+        {
+            _id:'6',
+            name: 'Slim-Pant',
+            category:'pants',
+            image: '/images/d4.jpg',
+            price: 40,
+            brand: 'Nike',
+            rating: 1.5,
+            status:'In stock',
+            numReviews: 10,
+            countInStock:4
+        }
+    ]
+}
